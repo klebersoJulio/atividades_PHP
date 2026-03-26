9e3599f78493f68b4c2fa87a8ce7d7ce68a1bc53
klebersoJulio <143755741+klebersoJulio@users.noreply.github.com>
2026-03-26 20:56:49 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,44 +1,39 @@
+<?php
+    // função recursiva com parametro ($n)
+    function calcularFatorial($n){
+        if($n <= 1){
+            return 1;
+        }
+        return $n * calcularFatorial($n-1);
+    }
+    $mensagem="";
+    if($_SERVER["REQUEST_METHOD"] == "POST" ){
+        $numero = intval($_POST['numero']);
+        if($numero <= 0){
+            $numero = "por favor, digite um número positivo";
+        }else{
+            $resul = calcularFatorial($numero);
+            $mensagem = "O fatorial de " .$numero. "é" .$resul;
+        }
+    }
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <title>meu formulario simples</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resultado</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-
-<div class="container">
-    <form method="POST" class="form-box">
-        <h2>qual a sua mensagem para o mundo? </h2>
-
-        <div class="input-group">
-            <input type="text" name="nome" required>
-            <label>qual é seu nome dog</label>
-        </div>
-
-        <div class="input-group">
-            <input type="email" name="email" required>
-            <label>fala seu emeil ai pai</label>
-        </div>
-
-        <div class="input-group">
-            <textarea name="mensagem" required></textarea>
-            <label>bota a msg aqui</label>
-        </div>
-
-        <button type="submit">Clique se puder </button>
-
-        <?php
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            $nome = htmlspecialchars($_POST["nome"]);
-            $email = htmlspecialchars($_POST["email"]);
-            $mensagem = htmlspecialchars($_POST["mensagem"]);
-
-            echo "<p class='sucesso'>Mensagem enviada, $nome! 👍</p>";
-        }
-        ?>
-    </form>
-</div>
-
-</body>
+    <div class="apple-card text-center">
+        <small class="text-secundary d-block mb-1">calculo efetuado</small>
+        <strong style="color:#1d1d1f;"><?php echo $mensagem?>
+    </div>
+    <br>
+    <a href="index.html" class="btn btn-apple w-100" style="text-decoration : none;">
+        Calcular Novamente
+</a>
+</body> 
 </html>
